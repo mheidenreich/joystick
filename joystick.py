a8e1097212530dfa65f5543c5ce941a06f908f17
mheidenreich <76790713+mheidenreich@users.noreply.github.com>
2021-05-04 15:12:57 -0400

    Update joystick.py

--- joystick.py
+++ joystick.py
@@ -36,14 +36,31 @@ def read_ads7830(input):
     bus.write_byte(0x4b, ads7830_commands[input])
     return bus.read_byte(0x4b)
 
+def no_drift(input):
+        value = read_ads7830(input)
+
+        return value if value < 110 else 127 
+
+def read_min(input):
+    while True:
+        value = read_ads7830(input)
+
+        yield (127-value)/127 if value < 110 else 0
+
+def read_max(input):
+    while True:
+        value = read_ads7830(input)
+
+        yield (value-128)/127 if value > 140 else 0
+
 try:
     signal(SIGTERM, safe_exit)
     signal(SIGHUP, safe_exit)
 
-    while True:
-        print(f"X: {read_ads7830(7)}, Y: {read_ads7830(6)}")
-
-        sleep(0.1)
+    top.source = read_max(6)
+    right.source = read_max(7)
+    bottom.source = read_min(6)
+    left.source = read_min(7)
 
     pause()
 
@@ -51,6 +68,10 @@ except KeyboardInterrupt:
     pass
 
 finally:
+    top.source = None
+    right.source = None
+    bottom.source = None
+    left.source = None
     top.close()
     right.close()
     bottom.close()
